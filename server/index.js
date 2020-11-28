38b97e33f070c5f596dd2d19dd2670589ae52643
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-28 20:43:41 +0530

    commit : port created on 5000 and mongoose connection on atlas

---
+++ server/index.js
@@ -0,0 +1,31 @@
+/** @format */
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+// @ts-ignore
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+// @ts-ignore
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
+const connection_url =
+  "mongodb+srv://harshal:harshal7797@test-cluster.1swf7.mongodb.net/demo?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+//connection
+mongoose
+  .connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`port running on port : ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log(`${error} did not connect`);
+  });
+mongoose.set("useFindAndModify", false);
